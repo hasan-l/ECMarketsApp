8cd07ebc5cafa50fb8e4871c399e05676417df6c
hasan-l <hasanl01@hotmail.co.uk>
2025-06-05 11:02:56 +0100

    Victory native charting package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}", // For App.js/App.tsx in the root
+    "./app/**/*.{js,jsx,ts,tsx}", // If using Expo Router with an 'app' directory
+    "./components/**/*.{js,jsx,ts,tsx}", // For your components
+    // Add any other directories/files where you use Tailwind classes
+  ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
