c1f34f87f97e7012b7be066d50df1f9387e48f20
hasan-l <hasanl01@hotmail.co.uk>
2025-06-05 13:09:08 +0100

    Remove header

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,17 @@
 // App.js
 import StockChartScreen from "@/components/StockChartScreen";
-import React from "react";
+import { useNavigation } from "expo-router";
+import React, { useLayoutEffect } from "react";
 import { SafeAreaView, StatusBar, View } from "react-native";
 import "../global.css";
 
 export default function App() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, [navigation]);
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <StatusBar barStyle="dark-content" />
